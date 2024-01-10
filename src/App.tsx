fe41fac1430168331289b05e007e8b9711a138f3
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-01-09 23:25:27 -0500

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import '../src/index.css'
 import 'katex/dist/katex.min.css';
+import Card from './Components/card';
 /*import Tex from '@matejmazur/react-katex'; */
 
 function App() {
   return (
-    <div className=' '>
+    <div>
+      <Card/>
     </div>
   )
 }
--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -1,9 +1,19 @@
-const card = () => {
-  return (
-    <div className="">
+import '../index.css'
 
+
+const Card = () => {
+  return (
+    <div className="flex flex-col mx-96 my-96 justify-center items-center max-w-2xl outline rounded-md border-cyan-200 text-white">
+        <div className="flex px-24">Title</div>
+        <div className="flex px-24">Problem</div>
+        <div className="grid grid-cols-2 border-solid outline">
+            <button className="border-solid outline">Choice 1</button>
+            <button className="border-solid outline">Choice 2</button>
+            <button className="border-solid outline">Choice 3</button>
+            <button className="border-solid outline">Choice 4</button>
+        </div>
     </div>
   )
 }
 
-export default card
+export default Card
