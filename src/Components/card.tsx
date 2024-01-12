3d42e5ca737d83326f87e5fe8d4241770ead38ec
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-01-12 13:16:50 -0500

    updated card

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -1,16 +1,18 @@
 import '../index.css'
-
+import Tex from '@matejmazur/react-katex'
 
 const Card = () => {
   return (
-    <div className="flex flex-col mx-96 my-96 justify-center items-center max-w-2xl outline rounded-md border-cyan-200 text-white">
-        <div className="flex px-24">Title</div>
-        <div className="flex px-24">Problem</div>
-        <div className="grid grid-cols-2 border-solid outline">
-            <button className="border-solid outline">Choice 1</button>
-            <button className="border-solid outline">Choice 2</button>
-            <button className="border-solid outline">Choice 3</button>
-            <button className="border-solid outline">Choice 4</button>
+    <div className="flex flex-col mx-96 my-96 outline rounded-md border-cyan-200 text-white">
+        <div className='flex flex-col text-center p-4'>
+          <p className='mb-2 break-words'>Title</p>
+          <p className='mt-2'><Tex block>x=3+2x</Tex></p>
+        </div>
+        <div className="grid grid-cols-2 p-4 gap-4 ">
+            <button className="grid border-solid outline rounded-md">Choice 1</button>
+            <button className="grid border-solid outline rounded-md">Choice 2</button>
+            <button className="grid border-solid outline rounded-md">Choice 3</button>
+            <button className="grid border-solid outline rounded-md">Choice 4</button>
         </div>
     </div>
   )
