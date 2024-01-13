a43bc373ac7a9f6f957bb520899483ab4fe511ee
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-01-13 16:56:56 -0500

    added more functionality and colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,34 @@ import '../src/index.css'
 import 'katex/dist/katex.min.css';
 import Card from './Components/card';
 import questions from './questions';
+import { useState } from 'react';
 
 
-const randomNumber = Math.floor(Math.random() * questions.length)
 
 function App() {
 
+  const [index, setIndex] = useState(1)
+  const [questionIndices, setQuestionIndices] = useState([...questions.keys()])
+
+  async function changeQuestion(){
+    console.log(questionIndices)
+    let indices = questionIndices
+    if (questionIndices.length === 0){
+      indices = [...questions.keys()]
+
+    }
+    const randomNumber = Math.floor(Math.random() * indices.length)
+    setIndex(indices[randomNumber])
+    setQuestionIndices(_ => indices.filter((_,i) => i!==randomNumber) )
+
+  }
+
+
   return (
     <div>
-      <div className='flex mx-80 mt-80 flex-col text-white'>
-        <Card index={randomNumber}/>
-        <button className='mt-4 outline rounded-md border-cyan-200'>Click</button>
+      <div className="flex flex-col text-white min-[1000px]:mt-56 min-[1000px]:mx-56">
+        <Card index={index}/>
+        <button className='mt-4 outline rounded-md border-cyan-200 bg-slate-700 text-3xl py-4' onClick={changeQuestion}>Click</button>
       </div>
     </div>
   )
--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -1,33 +1,61 @@
 import '../index.css'
 import Tex from '@matejmazur/react-katex'
 import dataStuff from '../../Questions/question.json'
-
+import { useEffect, useState } from 'react'
 
 interface Question{
   title:string
   problem:string
   options:string[]
+  correct:number
 }
 
 
 const questions = dataStuff as Question[]
 
 
+
 const Card = (props:{index:number}) => { 
 
+  const [content, setContent] = useState("")
+
+  function reply(ind:number){
+    if (ind === questions[props.index].correct){
+      console.log("working")
+      setContent("This is correct")
+    }
+    else{
+      setContent("This is false")
+    }
+  }
+
+  useEffect(() => {
+    setContent("")
+  },[questions[props.index].title])
+
+  function renderButtons(){
+    const buttons = []
+    for(let [index,option] of questions[props.index].options.entries()){
+      buttons.push(<button key={index} className="grid border-solid outline rounded-md" onClick={() => reply(index)}>{option}</button>)
+      console.log(<button key={index} className="grid border-solid outline rounded-md" onClick={() => reply(index)}>{option}</button>)
+      
+      }
+    return buttons
+  }
+
   return (
-    <div className='outline rounded-md border-cyan-200'>
-        <div className='flex flex-col text-center p-4'>
-          <p className='mb-2 break-words'>{questions[props.index].title}</p>
-          <p className='mt-2'><Tex block>{questions[props.index].problem}</Tex></p>
+    <>
+    <div className="outline rounded-md border-cyan-200 bg-slate-700">
+        <div className='flex flex-col text-center text-2xl'>
+          <p className='mb-2 break-words bg-cyan-500'>{questions[props.index].title}</p>
+          <div className='mt-2'><Tex block>{questions[props.index].problem}</Tex></div>
         </div>
         <div className="grid grid-cols-2 p-4 gap-4 ">
-            <button className="grid border-solid outline rounded-md">{questions[props.index].options[0]}</button>
-            <button className="grid border-solid outline rounded-md">{questions[props.index].options[1]}</button>
-            <button className="grid border-solid outline rounded-md">{questions[props.index].options[2]}</button>
-            <button className="grid border-solid outline rounded-md">{questions[props.index].options[3]}</button>
+          {renderButtons()}
         </div>
+        <p className="text-center my-10">{content}</p>
     </div>
+    </>
   )
 }
 
